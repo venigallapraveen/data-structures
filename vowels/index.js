a06c51e9dbe4374df02df7dd6ac8245c0b603eee
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-18 16:26:49 -0400

    completed finding vowels solution

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = "aeiouAEIOU";
+
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = vowels;
